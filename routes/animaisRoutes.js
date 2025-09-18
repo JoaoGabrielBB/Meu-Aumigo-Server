6f01276a4ce8085af6ddf4bea17834206bbc6640
JoaoGabrielBB <joaodarksouls3dasilva@gmail.com>
2025-09-17 21:38:30 -0300

    aaaaaaaaahhhhhh

--- routes/animaisRoutes.js
+++ routes/animaisRoutes.js
@@ -1,7 +1,7 @@
 import express from "express";
 import AnimalController from "../controllers/AnimaisController.js"; // relativo a routes
-import upload from "..middlewares/upload.js";
-import paginar from "..middlewares/paginar.js";
+import upload from "../middlewares/upload.js";
+import paginar from "../middlewares/paginar.js";
 
 const router = express.Router();
 
